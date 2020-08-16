c097ca1b0d338ad2f60ed3d97ed6f102279ee360
lizyang9538 <yy4@cs.cmu.edu>
2020-08-15 20:47:48 -0400

    add colab config

--- src/train_ltd.py
+++ src/train_ltd.py
@@ -12,9 +12,14 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
 
 if __name__ == '__main__':
-    chkpt_dir = os.path.join(config.checkpoint_dir, 'fine_tune') \
-        if config.fine_tune else \
-        os.path.join(config.checkpoint_dir, 'stage_%02d' % config.stage)
+    if config.use_colab:
+        chkpt_dir = os.path.join(config.checkpoint_dir_colab, 'fine_tune') \
+            if config.fine_tune else \
+            os.path.join(config.checkpoint_dir_colab, 'stage_%02d' % config.stage)
+    else:
+        chkpt_dir = os.path.join(config.checkpoint_dir, 'fine_tune') \
+            if config.fine_tune else \
+            os.path.join(config.checkpoint_dir, 'stage_%02d' % config.stage)
     if not os.path.exists(chkpt_dir):
         os.makedirs(chkpt_dir)
     model = Deblur(config.num_stacks)
@@ -25,7 +30,8 @@ if __name__ == '__main__':
     scheduler = torch.optim.lr_scheduler.StepLR(
         optimizer, step_size=config.step_size)
 
-    trainset = BlurImgDataset(config.train_dir)
+    trainset = BlurImgDataset(config.train_dir_colab if config.use_colab \
+                                                    else config.train_dir)
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=config.batch_size, shuffle=True,
         num_workers=config.batch_size)
@@ -36,10 +42,13 @@ if __name__ == '__main__':
     it = 0
     for epoch in range(config.max_epoch):
         for i, (blur, ker, img) in enumerate(trainloader):
-            blur = blur.to(config.device)
+            if config.use_colab:
+                blur = blur.to(config.device)
+                ker_gt = ker.to(config.device)
+            else:
+                ker_gt = ker
+
             im_pred, k_pred = model(blur)
-            # ker_gt = ker
-            ker_gt = ker.to(config.device)
             optimizer.zero_grad()
             loss = op.shift_invariant_mse(im_pred, img)
             loss.backward()
